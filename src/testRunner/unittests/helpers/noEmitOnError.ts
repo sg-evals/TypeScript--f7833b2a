e345877a04fcc9cd8a640e5ebb17e5c85b5678d6
sg-evals <benchmarks@sourcegraph.com>
2026-03-29 15:27:11 -0400

    Mirror of microsoft/TypeScript at f7833b2a72309dd695b45cf2cf2187e2f2f264df
    Upstream: https://github.com/microsoft/TypeScript
    Ref: f7833b2a72309dd695b45cf2cf2187e2f2f264df

---
+++ src/testRunner/unittests/helpers/noEmitOnError.ts
@@ -0,0 +1,60 @@
+import { dedent } from "../../_namespaces/Utils.js";
+import { jsonToReadableText } from "../helpers.js";
+import {
+    FsContents,
+    libContent,
+} from "./contents.js";
+import { libFile } from "./virtualFileSystemWithWatch.js";
+
+function getFsContentsForNoEmitOnError(outFile: boolean, declaration: true | undefined, incremental: true | undefined): FsContents {
+    return {
+        "/user/username/projects/noEmitOnError/tsconfig.json": jsonToReadableText({
+            compilerOptions: {
+                ...outFile ? { outFile: "../dev-build.js", module: "amd" } : { outDir: "./dev-build" },
+                declaration,
+                incremental,
+                noEmitOnError: true,
+            },
+        }),
+        "/user/username/projects/noEmitOnError/shared/types/db.ts": dedent`
+            export interface A {
+                name: string;
+            }
+        `,
+        "/user/username/projects/noEmitOnError/src/main.ts": dedent`
+            import { A } from "../shared/types/db";
+            const a = {
+                lastName: 'sdsd'
+            ;
+        `,
+        "/user/username/projects/noEmitOnError/src/other.ts": dedent`
+            console.log("hi");
+            export { }
+        `,
+        [libFile.path]: libContent,
+    };
+}
+
+export function forEachNoEmitOnErrorScenario<T>(
+    loadFs: (contents: FsContents, currentDirectory: string, executingFilePath: string) => T,
+    action: (
+        scenarioName: (scenario: string) => string,
+        fs: () => T,
+    ) => void,
+) {
+    for (const outFile of [false, true]) {
+        for (const declaration of [undefined, true] as const) {
+            for (const incremental of [undefined, true] as const) {
+                action(
+                    scenario => `${outFile ? "outFile" : "multiFile"}/${scenario}${declaration ? " with declaration" : ""}${incremental ? " with incremental" : ""}`,
+                    () =>
+                        loadFs(
+                            getFsContentsForNoEmitOnError(outFile, declaration, incremental),
+                            "/user/username/projects/noEmitOnError",
+                            libFile.path,
+                        ),
+                );
+            }
+        }
+    }
+}
